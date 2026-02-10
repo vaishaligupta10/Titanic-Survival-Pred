7c52427ca3dc1c3d20cec1124e67a6df60c2ba30
vaishali gupta <vaishaligupta10072006@gmail.com>
2026-02-10 21:38:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(max_iter=1000 , solver="liblinear")
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
