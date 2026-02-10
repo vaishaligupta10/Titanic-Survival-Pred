4feb38824a3fa7fe3b48c9cedde78a125445acd4
vaishali gupta <vaishaligupta10072006@gmail.com>
2026-02-10 21:13:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+
+df = pd.read_csv('Titanic-Dataset.csv')
+
+df['Age'].fillna(df['Age'].median(), inplace=True)
+df['Fare'].fillna(df['Fare'].median(), inplace=True)
+df.dropna(subset=['Embarked'], inplace=True)
+
+df = pd.get_dummies(df, columns=['Sex', 'Embarked'], drop_first=True)
+
+X = df[['Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex_male',
+        'Embarked_Q', 'Embarked_S']]
+y = df['Survived']
+
+df.head()
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+print("Accuracy:", round(accuracy_score(y_test, y_pred), 3))
+
+cm = confusion_matrix(y_test, y_pred)
+
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.tight_layout()
+plt.show()
